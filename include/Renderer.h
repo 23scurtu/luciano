6dd8426e31c3175e67395792930dabfc783cd426
23scurtu <23scurtuvictor@gmail.com>
2018-05-20 20:38:00 -0700

    Initial commit

---
+++ include/Renderer.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+
+class Renderer
+{
+    public:
+    Renderer();
+
+    int init();
+};
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Renderer.h"
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+    Renderer renderer;
+    renderer.init();
+}
