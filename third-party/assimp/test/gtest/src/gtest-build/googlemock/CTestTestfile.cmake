102cd110f54f85c8b6de7d19b891e7d4be196913
23scurtu <23scurtuvictor@gmail.com>
2018-06-27 00:56:20 -0700

    Update assimp and CMakeLists.txt

--- third-party/assimp/test/gtest/src/gtest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vision/Desktop/luciano/third-party/assimp/test/gtest/src/gtest/googlemock
-# Build directory: /home/vision/Desktop/luciano/third-party/assimp/test/gtest/src/gtest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- third-party/assimp/test/gtest/src/gtest-build/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vision/Desktop/luciano/third-party/assimp/test/gtest/src/gtest/googletest
-# Build directory: /home/vision/Desktop/luciano/third-party/assimp/test/gtest/src/gtest-build/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
